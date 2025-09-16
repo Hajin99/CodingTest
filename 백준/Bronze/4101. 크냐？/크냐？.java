69929c1fbd3027e41735aa64c3b7283aa48cd3f9
김하진 <114723246+Hajin99@users.noreply.github.com>
2025-09-16 17:39:04 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -7,18 +7,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+        int a;
+        int b;
 
-        while(a!=0 && b!=0) {
+        do{
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            if(a==0 && b==0){
+                break;
+            }
+            
             if(a>b)
                 System.out.println("Yes");
             else
                 System.out.println("No");
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-        }
+        } while(true);
     }
 }
